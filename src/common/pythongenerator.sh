90ae2ac7b1cc48cdcb9266746ea37f5db0a1539f
jeandet <jeandet@localhost>
2014-06-15 11:28:36 +0200

    Sync /!\ do not use this revision

--- src/common/pythongenerator.sh
+++ src/common/pythongenerator.sh
@@ -3,4 +3,4 @@
 #export QTDIR=/usr/include
 #export QTDIR=/usr/include/qt5
 
-pythonqt_generator  --include-paths=./elf:./srec:./BinFile:/usr/include/qt5:/usr/include/qt5/QtCore:/usr/include/qt5/QtWidgets  --output-directory=pythonQtOut PySocExplorer.h pythonQtgeneratorCfg.txt
+pythonqt_generator  --include-paths=./genericBinaryFiles/elf:./genericBinaryFiles/srec:./genericBinaryFiles/BinFile:/usr/include/qt5:/usr/include/qt5/QtCore:/usr/include/qt5/QtWidgets  --output-directory=pythonQtOut PySocExplorer.h pythonQtgeneratorCfg.txt
