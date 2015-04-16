d3aba6931907f1a5acfa285065e93fa43bede791
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2015-04-16 20:01:01 +0200

    added int ElfFile::getSymbolIndex(const QString &name)

--- doc/PythonExamples/PythonPlugin.py
+++ doc/PythonExamples/PythonPlugin.py
@@ -1,4 +1,5 @@
-#!/usr/bin/lppmon -e 
+#!/usr/bin/lppmon -e
+
 
 class test(PySocExplorerEngine.socexplorerplugin):
     m_Layout = QtGui.QGridLayout()
@@ -9,23 +10,27 @@ class test(PySocExplorerEngine.socexplorerplugin):
     m_writeQPB = QtGui.QPushButton("write")
 
     def postInstantiationTrigger(self):
-        print "building GUI"
+        print("building GUI")
         self.setWidget(self.m_CenterWidget)
         self.m_CenterWidget.setLayout(self.m_Layout)
-        self.m_Layout.addWidget(self.m_addressQSPB,0,0,1,1)
-        self.m_Layout.addWidget(self.m_readQPB,0,1,1,1)
-        self.m_Layout.addWidget(self.m_writeQPB,0,2,1,1)
-        self.m_Layout.addWidget(self.m_hexviewer,1,0,1,-1)
+        self.m_Layout.addWidget(self.m_addressQSPB, 0, 0, 1, 1)
+        self.m_Layout.addWidget(self.m_readQPB, 0, 1, 1, 1)
+        self.m_Layout.addWidget(self.m_writeQPB, 0, 2, 1, 1)
+        self.m_Layout.addWidget(self.m_hexviewer, 1, 0, 1, -1)
+
     def hello(self):
-        print "hello"
+        print("hello")
+
     def VID(self):
         return 10
+
     def PID(self):
         return 100
+
     def readData(self):
         bufferQBA = QtCore.QByteArray()
-        buffer = self.parentPlugin().Read(self.m_addressQSPB.value,16*1024)
-        for i in range(0,16*1024):
+        buffer = self.parentPlugin().Read(self.m_addressQSPB.value, 16*1024)
+        for i in range(0, 16*1024):
             bufferQBA.append(int(buffer[i]))
             bufferQBA.append(int(buffer[i]))
             bufferQBA.append(int(buffer[i]))
